f270dd517340a6099cda01d1b44a0a1552d8e0b6
lixy3 <45132236+lixy3@users.noreply.github.com>
2020-09-18 13:20:22 +0100

    Add files via upload

---
+++ pag3script.js
@@ -0,0 +1,24 @@
+
+//init();
+document.querySelector('.btn-roll').addEventListener('click', function() {
+        var dice1 = Math.floor(Math.random() * 6) + 1;
+        var dice2 = Math.floor(Math.random() * 6) + 1;
+        document.getElementById('dice-1').style.display = 'block';
+        document.getElementById('dice-2').style.display = 'block';
+        document.getElementById('dice-1').src = 'dice-' + dice1 + '.png';
+        document.getElementById('dice-2').src = 'dice-' + dice2 + '.png';
+        console.log(dice1 + dice2);
+        if(dice1 + dice2 < 8)
+        {
+          window.location.replace("pag1.html");
+      }else {
+          window.location.replace("pag4.html");
+        }
+});
+
+function init() {
+
+
+    document.getElementById('dice-1').style.display = 'none';
+    document.getElementById('dice-2').style.display = 'none';
+}
