a6631490c12e108399f6e9b2a7cdf70c32f1f872
Razvan Popescu <razvanpopescu@Razvans-MacBook-Pro.local>
2020-09-19 10:59:11 +0100

    Razvan's  Work

---
+++ Page1/script1.js
@@ -0,0 +1,12 @@
+function myFunction() {
+    document.getElementById("demo1").style.visibility = "visible";
+ 
+}
+
+function myFunction1() {
+    document.getElementById("demo2").style.visibility = "visible";
+}
+
+document.addEventListener("contextmenu", function (e) {
+    e.preventDefault();
+  });
